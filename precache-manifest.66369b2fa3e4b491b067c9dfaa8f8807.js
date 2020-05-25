727a66647fbb14845a7aa3fd8cf0ad11ad74aa7c
Shail <raval.shail1@gmail.com>
2020-05-25 20:08:12 +0530

    Updates

---
+++ precache-manifest.66369b2fa3e4b491b067c9dfaa8f8807.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b5d3bf9d05a75c96ec51c092a57bce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d466ccf1ce323423a97",
+    "url": "/static/css/main.9bc77392.chunk.css"
+  },
+  {
+    "revision": "0e7e1f2d220c042bef85",
+    "url": "/static/js/2.85b89bc6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.85b89bc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d466ccf1ce323423a97",
+    "url": "/static/js/main.70e05f2d.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "45f66c63c9688fe7864c96e2ca071679",
+    "url": "/static/media/VectorX_cover.45f66c63.jpeg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/speedwell/precache-manifest.64b565af638123df628610850ee76ea6.js"
+  "/precache-manifest.66369b2fa3e4b491b067c9dfaa8f8807.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/speedwell/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
