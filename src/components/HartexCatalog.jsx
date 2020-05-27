a5bb5b37e70c394b285bb8456fe244cde1355e4f
Shail <raval.shail1@gmail.com>
2020-05-27 16:57:21 +0530

    'change'

---
+++ src/components/HartexCatalog.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class HartexCatalog extends Component {
+  render(){
+    return (
+    <div className="HartexCatalog">
+        <h1>This page is under construction. Contact us for any query</h1>
+    </div>
+  );
+}
+}
+
+export default HartexCatalog;
--- src/components/VectorCatalog.jsx
+++ src/components/VectorCatalog.jsx
@@ -5,7 +5,7 @@ class VectorCatalog extends Component {
   render(){
     return (
     <div className="VectorCatalog">
-        Hello
+        <h1>This page is under construction. Contact us for any query</h1>
     </div>
   );
 }
